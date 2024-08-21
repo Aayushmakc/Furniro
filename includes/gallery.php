f2318b9588973a9d496c24b9e1d58ca0269079c2
Aayushmakc <rojikc764@gmail.com>
2024-08-21 17:08:31 +0545

    Added banner image and products

--- includes/gallery.php
+++ includes/gallery.php
@@ -1,12 +1,62 @@
 <section class="index-gallery section-padding">
 
-<div class="container">
-  <div class="gallery-text">
-    <h6 class="sub-heading">Share your setup with</h6>
-    <h1  class="block-heading">#FurnioFurniture</h1>
-  </div>
- <div class="image-grid">
- 
+  <div class="container">
+    <div class="gallery-text">
+      <h6 class="sub-heading">Share your setup with</h6>
+      <h1 class="block-heading">#FurnioFurniture</h1>
+    </div>
+    <div class="image-grid">
+      <!-- Row one -->
+    <div class="row">
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle36.png" class="card-img-top image-one" alt="Image1">
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle38.png" class="card-img-top image-two" alt="Image2">
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle40.png" class="card-img-top image-three" alt="Image3">
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle43.png" class="card-img-top image-four" alt="Image4">
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle45.png" class="card-img-top image-five" alt="Image5">
+            </div>
+        </div>
+    </div>
+    <!-- Row two -->
+    <div class="row">
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle37.png" class="card-img-top image-six" alt="Image1">
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle39.png" class="card-img-top image-seven" alt="Image2">
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle41.png" class="card-img-top image-eight" alt="Image3">
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <img src="./images/Rectangle44.png" class="card-img-top image-nine" alt="Image4">
+            </div>
+        </div>
+    </div>
 
-</div>
+    </div>
 </section>
